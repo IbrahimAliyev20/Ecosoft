a988d657eb245ca791a0d1992321ad267310277a
IbrahimAliyev20 <iboisa12@gmail.com>
2025-07-03 14:33:53 +0400

    lang

--- src/app/[locale]/products/[slug]/ProductInfo.tsx
+++ src/app/[locale]/products/[slug]/ProductInfo.tsx
@@ -1,23 +1,47 @@
-'use client';
+"use client";
 
-import { Phone } from 'lucide-react';
-import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '@/components/ui/accordion'; 
-import { Button } from '@/components/ui/button';
-import Image from 'next/image';
-import { AttributeType, ProductType } from '@/types/alltype'; 
-export function ProductInfo({ atribute, product }: { product: ProductType, atribute: AttributeType[] }) {
+import { Phone } from "lucide-react";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import { AttributeType, ProductType } from "@/types/alltype";
+import { Link } from "@/i18n/navigation";
+export function ProductInfo({
+  atribute,
+  product,
+}: {
+  product: ProductType;
+  atribute: AttributeType[];
+}) {
   return (
     <div>
-      <div className='border p-6 rounded-2xl shadow-sm w-full '>
-        <p className="text-sm font-medium text-cyan-600 mb-1">{product.title}</p> 
-        
-        <h1 className="text-3xl lg:text-4xl font-semibold text-gray-800 mb-6">{product.name}</h1> 
-        
+      <div className="border p-6 rounded-2xl shadow-sm w-full ">
+        <p className="text-sm font-medium text-cyan-600 mb-1">
+          {product.title}
+        </p>
+
+        <h1 className="text-3xl lg:text-4xl font-semibold text-gray-800 mb-6">
+          {product.name}
+        </h1>
+
         <hr className="border-gray-200" />
 
-        <Accordion type="single" collapsible className="w-full mt-6 border-none">
+        <Accordion
+          type="single"
+          collapsible
+          className="w-full mt-6 border-none"
+        >
           {atribute.map((item, index) => (
-            <AccordionItem key={index} value={`item-${index}`} className="border rounded-xl mb-3">
+            <AccordionItem
+              key={index}
+              value={`item-${index}`}
+              className="border rounded-xl mb-3"
+            >
               <AccordionTrigger className="font-semibold text-base hover:no-underline p-4">
                 <div className="flex items-center gap-3">
                   <div className="bg-cyan-500/10 p-2 rounded-full w-10 h-10">
@@ -27,7 +51,8 @@ export function ProductInfo({ atribute, product }: { product: ProductType, atrib
                       width={30}
                       height={30}
                       priority={true}
-                    />  
+                    />{" "}
+                     
                   </div>
                   {item.title}
                 </div>
@@ -39,25 +64,38 @@ export function ProductInfo({ atribute, product }: { product: ProductType, atrib
           ))}
         </Accordion>
 
-        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 mt-6">
-          <Button 
-            variant={"default"}
-            size="lg" className="bg-[#10B981] hover:bg-[#059669] flex items-center gap-2 rounded-xl">
-            <Image 
-              src='/icons/Social Media Icon.svg'
-              alt='whatsapp icon'
-              width={20}
-              height={20}
-              priority={true}
-              className='text-white '
-            />
-            Whatsappla əlaqə
-          </Button>
-          <Button 
-            variant={"default"}
-            size="lg" className="bg-[#06B6D4] hover:bg-[#0891B2] text-white flex items-center gap-2 rounded-xl">
-            <Phone size={20} /> Bizimlə əlaqə
-          </Button>
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 mt-6 w-full">
+          <Link
+            href="https://wa.me/994102565007"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="w-full"
+          >
+            <Button
+              variant={"default"}
+              size="lg"
+              className="w-full bg-[#10B981] hover:bg-[#059669] flex items-center gap-2 rounded-xl"
+            >
+              <Image
+                src="/icons/Social Media Icon.svg"
+                alt="whatsapp icon"
+                width={20}
+                height={20}
+                priority={true}
+                className="text-white "
+              />
+              Whatsappla əlaqə
+            </Button>
+          </Link>
+          <Link href="/contact" className="w-full">
+            <Button
+              variant={"default"}
+              size="lg"
+              className="w-full bg-[#06B6D4] hover:bg-[#0891B2] text-white flex items-center gap-2 rounded-xl"
+            >
+              <Phone size={20} /> Bizimlə əlaqə
+            </Button>
+          </Link>
         </div>
       </div>
     </div>

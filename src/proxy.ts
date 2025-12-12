b2e17d18b855edb264be4b0341987fa8afe4f671
IbrahimAliyev20 <iboisa12@gmail.com>
2025-12-12 15:35:50 +0400

    Update dependencies and add middleware for internationalization. Upgraded Next.js to version 16.0.10, React to 19.2.3, and next-intl to 4.5.8. Introduced a new proxy middleware for routing in internationalization. Adjusted TypeScript configuration for improved JSX support and included additional type definitions.

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,8 +1,9 @@
 import {routing} from '@/i18n/routing';
 import createMiddleware from 'next-intl/middleware';
  
-export default createMiddleware(routing);
+export const proxy = createMiddleware(routing);
  
 export const config = {
   matcher: '/((?!api|trpc|_next|_vercel|.*\\..*).*)'
 };
+
